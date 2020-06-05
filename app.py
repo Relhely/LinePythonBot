801813daf03e85ecd0cb58f9984291f16ff97bf6
Relhely <59365352+Relhely@users.noreply.github.com>
2020-06-05 23:36:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from datetime import datetime
 import gspread
 import random
 from oauth2client.service_account import ServiceAccountCredentials
+import requests
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 
@@ -87,6 +90,24 @@ def wcat():
             gray = gray + 1
     return str(gold) + "金袍" + str(gray) + "銀袍"  + "\n" + "(" + str(win) + "限 " + str(ggold) + "選拔" + ")"    
 
+def bsnews():
+    title = []
+    html = []
+    news = "勤益科大首頁公告 : \n\n"
+    r = requests.get('https://www.ncut.edu.tw/')
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for i in range(1,5):
+            qwe = '#Dyn_2_1 > div > div > section > div > div:nth-child( %d ) > div > div.d-txt > div >a'%(i)
+            stories = soup.select(qwe)
+            for s in stories:
+                title.append(str(s.text.lstrip().rstrip()))
+                html.append(s.get('href'))
+    for x in range(4):
+        news += ("標題 : " + title[x] + '\n' + "連結 : " + html[x] + '\n\n')
+
+    return news
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -114,17 +135,12 @@ def handle_message(event):
         reply_text = prize()
     elif(text == "A抽卡"):
         reply_text = wcat()
-    #else:
-      #  reply_text = text
+    else:(text == "勤益公告")
+        reply_text = bsnews()
         
     
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
-
-
-uid = 'Uc791e422591cfd25826415ce497c0847'
-
-
                               
 import os
 if __name__ == "__main__":
