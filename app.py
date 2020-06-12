b9c431f3637aa04f59741f6e1b5de202261df143
Relhely <59365352+Relhely@users.noreply.github.com>
2020-06-12 10:20:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,7 @@ def handle_message(event):
     
     if(text == "!抽卡"):
         reply_text = wcat()
-    elif(text == "123test"):
-        reply_text = event.source.user_id
-    
+        
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
                               
