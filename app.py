e916ea5fadaa77e15cd2536c42299e347dcec24c
Relhely <59365352+Relhely@users.noreply.github.com>
2020-06-12 10:00:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
+    uid = event.source.user_id
     if(text == "!抽卡"):
-        reply_text = wcat()        
+        reply_text = wcat() 
+    elif(text = id查詢)
+        reply_text = uid
     
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
